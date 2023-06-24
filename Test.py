9307a36dc1628b9367aad41ce248d5c00ec48b20
ArPaVa <arianpzv@gmail.com>
2023-06-24 15:21:31 +0200

    Chase The Light! (the song before jump around)

--- Test.py
+++ Test.py
@@ -6,9 +6,12 @@ pygame.init()
 
 #a = Utils.Map(8,8)
 #b = Utils.Player(*a.random_non_ocuppied_pos())
-c = Utils.Game(8,8)
-
+c = Utils.Game(90,160)
+screen = pygame.display.set_mode(c.settings.provide_settings("resolution"))
 while True:
-    time.sleep(1)
-    c.paint()
-    pygame.display.update()
+    dump = pygame.event.get() #FUCKING PYGAME AND WINDOWS FUCKINGLE THINK PYGAME IS NOT RESPONDING IF YOU DONT INTERACT WITH THE EVENTS FOR TOO LONG
+    time.sleep(0.3)
+    keys = pygame.key.get_pressed()
+    c.recieve_input(keys)
+    c.paint(screen)
+    pygame.display.flip()
--- Utils.py
+++ Utils.py
@@ -2,34 +2,50 @@ import numpy as np
 import pygame
 import pickle
 
-
+# NOTA X es vertical y Y horizontaL
 class Map:  #have a map
+
     def __init__(self,x_size,y_size):
-        self.map_matrix = np.zeros(shape=(x_size,y_size),dtype=int)
-        for i in range(self.map_matrix.shape[0]):
-            if i>=  self.map_matrix.shape[1]/2:
-                for j in range(self.map_matrix.shape[1]):
-                    self.map_matrix[i,j]= 1
+        self.map_matrix = np.zeros(shape=(x_size,y_size),dtype=Block)
+        for i in range(self.map_matrix.shape[0]):         
+            for j in range(self.map_matrix.shape[1]):
+                if i>=  self.map_matrix.shape[0]/2:
+                    self.map_matrix[i,j]= Dirt()
+                else:
+                    self.map_matrix[i,j]= Air()
+            
         print(self.map_matrix)
+    
+    
+    def valid_posc(self,x,y):
+        if x>-1 and x < self.map_matrix.shape[0] and y>-1 and y < self.map_matrix.shape[1]:
+            return True
+        return False
+
     def random_non_ocuppied_pos(self):
         #TODO I know
         for i in range(self.map_matrix.shape[0]):
             for j in range(self.map_matrix.shape[1]):
-                if self.map_matrix[i,j]==0:
-                    return i,j
+                if isinstance(self.map_matrix[i,j],Air):
+                    #return i,j
+                    return 0,2
     #TODO Maybe make subscriptable giving the map_matrix thingy
 
 class Player:
+
     def __init__(self,initial_x, initial_y):
         self.pos_x = initial_x
         self.pos_y = initial_y
+
     def act_map (self, map:Map):
         #not sure if is going to work that way
         pass
+
     def __str__(self):
         return f"{self.pos_x},{self.pos_y}"
     
 class Settings:
+
     def __init__(self):
         try:
             file = open("settings.bin","rb")
@@ -66,38 +82,79 @@ class Settings:
         #TODO Checking if the settings are valid
         return True
         
+class Block:
+    density = 1
+    breakable = True
+    directory = None
+    def __repr__(self) -> str:
+        return self.__str__()
 
+class Dirt (Block):
+    density = 1
+    breakable = True
+    directory = (150,100,100)
+    def __str__(self) -> str:
+        return 'D'
+
+class Air(Block):
+    density = 0
+    breakable = False
+    directory = (30,30,100)
+    def __str__(self) -> str:
+        return 'A'
 class Game:
     def __init__(self,map_x_size,_map_y_size):
         self.settings = Settings()
         self.map = Map(map_x_size,_map_y_size)
         self.player = Player(*self.map.random_non_ocuppied_pos())
-    def paint(self):
-        pygame.init()
-        window = pygame.display.set_mode(self.settings.provide_settings("resolution"))
-        window.fill((255,255,255))
+        #self.screen = pygame.display.set_mode(self.settings.provide_settings("resolution"))
+        self.move_ticker = 0
+    def paint(self,window):
+        #window = pygame.Surface(self.settings.provide_settings("resolution"))
         temporal_materials = dict()
         temporal_materials[0] = (30,30,100)
         temporal_materials[1] = (150,100,100)
         temporal_materials[-1]= (150,70,70)
         
-        # x_size_of_rect = self.settings.provide_settings("resolution")[0]/self.map.map_matrix.shape[0]
-        # y_size_of_rect = self.settings.provide_settings("resolution")[1]/self.map.map_matrix.shape[1]
-        # x_center_of_rect = x_size_of_rect/2
-        # y_center_of_rect = y_size_of_rect/2
+        # rect_height = self.settings.provide_settings("resolution")[0]/self.map.map_matrix.shape[0]
+        # rect_width = self.settings.provide_settings("resolution")[1]/self.map.map_matrix.shape[1]
+        # x_center_of_rect = rect_height/2
+        # y_center_of_rect = rect_width/2
         # for x in range(self.map.map_matrix.shape[0]):
         #     for y in range(self.map.map_matrix.shape[1]):
         #         color = temporal_materials[self.map.map_matrix[x,y]]
-        #         pygame.draw.rect(window,color,pygame.Rect((x_center_of_rect*x,y_center_of_rect*y),(x_size_of_rect,y_size_of_rect)))
-        x_size_of_rect = self.settings.provide_settings("resolution")[0]/self.map.map_matrix.shape[0]
-        y_size_of_rect = self.settings.provide_settings("resolution")[1]/self.map.map_matrix.shape[1]
+        #         pygame.draw.rect(window,color,pygame.Rect((x_center_of_rect*x,y_center_of_rect*y),(rect_height,rect_width)))
+        rect_height = self.settings.provide_settings("resolution")[1]/self.map.map_matrix.shape[0]
+        rect_width = self.settings.provide_settings("resolution")[0]/self.map.map_matrix.shape[1]
+       
+       
         #drawing the map
         for x in range(self.map.map_matrix.shape[0]):
             for y in range(self.map.map_matrix.shape[1]):
-                color = temporal_materials[self.map.map_matrix[x,y]]
-                pygame.draw.rect(window,color,pygame.Rect((x_size_of_rect*y,y_size_of_rect*x),(x_size_of_rect,y_size_of_rect)))
+                pygame.draw.rect(window,self.map.map_matrix[x,y].directory,pygame.Rect((rect_height*y,rect_width*x),(rect_height,rect_width)))
+        
+        
         #drawing the player 
-        pygame.draw.rect(window,temporal_materials[-1],pygame.Rect((x_size_of_rect*self.player.pos_y,y_size_of_rect*self.player.pos_x),(x_size_of_rect,y_size_of_rect)))
+        pygame.draw.rect(window,temporal_materials[-1],pygame.Rect((rect_height*self.player.pos_y,rect_width*self.player.pos_x),(rect_height,rect_width)))
         #pygame.draw.rect(window,(0,0,255),[100,100,400,100],2)
-        return window
+        #self.screen = window
+        
 
+    def recieve_input(self,keys):
+        if self.move_ticker == 0:
+            if keys[pygame.K_LEFT]:
+                    self.move_ticker = 2
+                    if self.player.pos_y !=0:
+                        self.player.pos_y -=1
+
+            if keys[pygame.K_RIGHT]:
+                    self.move_ticker = 2  
+                    if self.player.pos_y != self.map.map_matrix.shape[1]-1:
+                        self.player.pos_y +=1
+            
+            #### This goes in a world method called player_movement or so, i am putting it here just to test the idea
+            if self.map.valid_posc(self.player.pos_x+1,self.player.pos_y) and self.map.map_matrix[self.player.pos_x+1,self.player.pos_y].density < 1:
+                self.move_ticker = 2
+                self.player.pos_x +=1
+        if self.move_ticker > 0:
+            self.move_ticker-=1
