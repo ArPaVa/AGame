ec61c587c52c148372427fe847197f9eee0f58ae
ArPaVa <arianpzv@gmail.com>
2023-06-28 08:34:00 -0400

    Digging Working, about to make it better....... or worse

--- Test.py
+++ Test.py
@@ -7,14 +7,23 @@ pygame.init()
 #a = Utils.Map(8,8)
 #b = Utils.Player(*a.random_non_ocuppied_pos())
 c = Utils.Game(90,160)
+gaming = True
 screen = pygame.display.set_mode(c.settings.provide_settings("resolution"))
-while True:
-    dump = pygame.event.get() #FUCKING PYGAME AND WINDOWS FUCKINGLE THINK PYGAME IS NOT RESPONDING IF YOU DONT INTERACT WITH THE EVENTS FOR TOO LONG
+while gaming:
+    events = pygame.event.get() #FUCKING PYGAME AND WINDOWS FUCKINGLE THINK PYGAME IS NOT RESPONDING IF YOU DONT INTERACT WITH THE EVENTS FOR TOO LONG
     time.sleep(0.05)
-    keys = pygame.key.get_pressed()
-    c.recieve_input(keys)
-    c.paint(screen)
-    check = pygame.mouse.get_pressed()
-    if check[0]:
-        c.on_click_dirt(pygame.mouse.get_pos())
-    pygame.display.flip()
+
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            gaming = False
+
+    if gaming:
+        keys_pressed = pygame.key.get_pressed()     
+        c.recieve_input_pressed(keys_pressed)
+        c.recieve_input_keydown(events)
+        c.paint(screen)
+        check = pygame.mouse.get_pressed()
+        if check[0]:
+            c.on_click_dirt(pygame.mouse.get_pos())
+        pygame.display.flip()
--- Utils.py
+++ Utils.py
@@ -36,23 +36,30 @@ class Player:
         self.pos_x = initial_x
         self.pos_y = initial_y
         self.going_up_list = []
+        self.digging = False
+        self.digging_iter = 10
+        self.digging_skill = 1
+        self.directory = (150,70,70)
+        
         #self.move_ticker = 0
 
     def move_around(self,keys,map:Map):
-        if keys[pygame.K_LEFT]:
-            #self.move_ticker = 2
-            move = self.actual_movement_coordinates(map,self.pos_x,self.pos_y-1)
-            self.pos_x += move[0]
-            self.pos_y += move[1]
+        if not self.digging:
 
-        if keys[pygame.K_RIGHT]:
-            #self.move_ticker = 2  
-            move = self.actual_movement_coordinates(map,self.pos_x,self.pos_y+1)
-            self.pos_x += move[0]
-            self.pos_y += move[1]
+            if keys[pygame.K_LEFT]:
+                #self.move_ticker = 2
+                move = self.actual_movement_coordinates(map,self.pos_x,self.pos_y-1)
+                self.pos_x += move[0]
+                self.pos_y += move[1]
+
+            if keys[pygame.K_RIGHT]:
+                #self.move_ticker = 2  
+                move = self.actual_movement_coordinates(map,self.pos_x,self.pos_y+1)
+                self.pos_x += move[0]
+                self.pos_y += move[1]
 
         #This just puts in a list the jump to be made it doesnt actually modify the posicion of the player
-        if keys[pygame.K_UP]:
+            if keys[pygame.K_UP]:
                 #self.move_ticker = 2
                 if map.map_matrix[(self.pos_x + 1),self.pos_y].density == 1:
                     self.going_up_list.clear()
@@ -61,8 +68,8 @@ class Player:
                     self.going_up_list.append(2)
                     self.going_up_list.append(2)
                     self.going_up_list.append(2)
-        
-        #This is where we actually handle the posicion of the player because is jumping    
+    
+    #This is where we actually handle the posicion of the player because is jumping    
         if len(self.going_up_list) > 0:
             jump = self.actual_movement_coordinates(map,(self.pos_x - self.going_up_list[0]),self.pos_y)
             self.pos_x += jump[0]
@@ -74,6 +81,73 @@ class Player:
             #self.move_ticker = 2
             self.pos_x +=1
        
+    def act_around(self,keys,map):
+        important_events = []
+        for events in keys:
+            if events.type == pygame.KEYDOWN:
+                if events.key == pygame.K_UP or events.key == pygame.K_DOWN or events.key == pygame.K_LEFT or events.key == pygame.K_RIGHT:
+                    if events not in important_events:
+                        self.latest_mov_key = events
+                        important_events.append(events)
+                    
+                if events.key == pygame.K_c:
+                    if pygame.K_c in important_events:
+                        important_events.remove(pygame.K_c)
+                    else:
+                        important_events.append(pygame.K_c)
+        if pygame.K_c in important_events:
+            important_events.remove(pygame.K_c)
+            if self.digging:
+                self.dig_skill()
+                self.directory = (150,70,70)
+            else:
+                if self.latest_mov_key.key==pygame.K_UP:
+                    self.dig_skill(map.map_matrix[self.pos_x-1,self.pos_y])
+                    self.block_to_break = (self.pos_x-1,self.pos_y)
+                    self.directory =(70,70,150)
+                if self.latest_mov_key.key==pygame.K_DOWN:
+                    self.dig_skill(map.map_matrix[self.pos_x+1,self.pos_y])
+                    self.block_to_break = (self.pos_x+1,self.pos_y)
+                    self.directory =(70,70,150)
+                if self.latest_mov_key.key==pygame.K_LEFT:
+                    self.dig_skill(map.map_matrix[self.pos_x,self.pos_y-1])
+                    self.block_to_break = (self.pos_x,self.pos_y-1)
+                    self.directory =(70,70,150)
+                if self.latest_mov_key.key==pygame.K_RIGHT:
+                    self.dig_skill(map.map_matrix[self.pos_x,self.pos_y+1])
+                    self.block_to_break = (self.pos_x,self.pos_y+1)
+                    self.directory =(70,70,150)
+                # for events in important_events:      
+                #     if events.key==pygame.K_UP:
+                #         self.dig_skill(map.map_matrix[self.pos_x-1,self.pos_y])
+                #         self.block_to_break = (self.pos_x-1,self.pos_y)
+                #         self.directory =(70,70,150)
+                #     if events.key==pygame.K_DOWN:
+                #         self.dig_skill(map.map_matrix[self.pos_x+1,self.pos_y])
+                #         self.block_to_break = (self.pos_x+1,self.pos_y)
+                #         self.directory =(70,70,150)
+                #     if events.key==pygame.K_LEFT:
+                #         self.dig_skill(map.map_matrix[self.pos_x,self.pos_y-1])
+                #         self.block_to_break = (self.pos_x,self.pos_y-1)
+                #         self.directory =(70,70,150)
+                #     if events.key==pygame.K_RIGHT:
+                #         self.dig_skill(map.map_matrix[self.pos_x,self.pos_y+1])
+                #         self.block_to_break = (self.pos_x,self.pos_y+1)
+                        # self.directory =(70,70,150)
+
+        if self.digging and ( self.digging_x_pos != self.pos_x or self.digging_y_pos != self.pos_y):
+                self.digging = False
+                self.directory =(150,70,70)
+        
+        if self.digging and self.digging_iter > 0:
+            self.digging_iter-=1
+        
+        if self.digging_iter == 0 and self.digging :
+            self.digging = False
+            self.directory =(150,70,70)
+            return ("break_block",self.block_to_break)
+            
+
     def actual_movement_coordinates(self,map,intended_x,intended_y):
         #TODO Propably is gonna have to change so it can return the posible position on laterall movements too, so in future Dashs or tps, it isnt necessary to make another method
         x = 0
@@ -108,9 +182,15 @@ class Player:
                         break
         return x,y
 
-    def dig_skill(self):
-        #TODO
-        pass
+    def dig_skill(self,block=None):
+        if not self.digging and block != None:
+            if block.density > 0:
+                self.digging = True
+                self.digging_iter = abs(10*block.density*self.digging_skill)
+                self.digging_x_pos = self.pos_x
+                self.digging_y_pos = self.pos_y
+        else:
+            self.digging = False
 
     def act_pos (self, map:Map):
         #not sure if is going to work that way
@@ -183,23 +263,11 @@ class Game:
         self.settings = Settings()
         self.map = Map(map_x_size,_map_y_size)
         self.player = Player(*self.map.random_non_ocuppied_pos())
+        self.orders = {"break_block":self.break_block}
         #self.screen = pygame.display.set_mode(self.settings.provide_settings("resolution"))
         self.move_ticker = 0
     def paint(self,window):
-        #window = pygame.Surface(self.settings.provide_settings("resolution"))
-        temporal_materials = dict()
-        temporal_materials[0] = (30,30,100)
-        temporal_materials[1] = (150,100,100)
-        temporal_materials[-1]= (150,70,70)
-        
-        # rect_height = self.settings.provide_settings("resolution")[0]/self.map.map_matrix.shape[0]
-        # rect_width = self.settings.provide_settings("resolution")[1]/self.map.map_matrix.shape[1]
-        # x_center_of_rect = rect_height/2
-        # y_center_of_rect = rect_width/2
-        # for x in range(self.map.map_matrix.shape[0]):
-        #     for y in range(self.map.map_matrix.shape[1]):
-        #         color = temporal_materials[self.map.map_matrix[x,y]]
-        #         pygame.draw.rect(window,color,pygame.Rect((x_center_of_rect*x,y_center_of_rect*y),(rect_height,rect_width)))
+      
         rect_height = self.settings.provide_settings("resolution")[1]/self.map.map_matrix.shape[0]
         rect_width = self.settings.provide_settings("resolution")[0]/self.map.map_matrix.shape[1]
        
@@ -211,10 +279,13 @@ class Game:
         
         
         #drawing the player 
-        pygame.draw.rect(window,temporal_materials[-1],pygame.Rect((rect_height*self.player.pos_y,rect_width*self.player.pos_x),(rect_height,rect_width)))
+        pygame.draw.rect(window,self.player.directory,pygame.Rect((rect_height*self.player.pos_y,rect_width*self.player.pos_x),(rect_height,rect_width)))
         #pygame.draw.rect(window,(0,0,255),[100,100,400,100],2)
         #self.screen = window
-        
+
+    def break_block(self,block_pos):
+        self.map.map_matrix[block_pos[0],block_pos[1]] = Air()
+
     def on_click_dirt(self,mouse_pos_tuple):
         rect_height = self.settings.provide_settings("resolution")[1]/self.map.map_matrix.shape[0]
         rect_width = self.settings.provide_settings("resolution")[0]/self.map.map_matrix.shape[1]
@@ -231,5 +302,11 @@ class Game:
 
         self.map.map_matrix[def_x,def_y] = Dirt()  
 
-    def recieve_input(self,keys):
-            self.player.move_around(keys,self.map)
+    def recieve_input_pressed(self,keys):
+        player_todo = self.player.move_around(keys,self.map)
+        
+    def recieve_input_keydown(self,keys):
+        player_todo = self.player.act_around(keys,self.map)
+        if player_todo != None:
+            self.orders[player_todo[0]](player_todo[1])
+
